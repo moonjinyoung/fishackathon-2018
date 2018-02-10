5582a2b74a4828b64456c925019e8d1a1df536c9
William To <williamto142@gmail.com>
2018-02-10 18:57:35 -0500

    stuffs

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var port = 8080 || process.env.PORT;
-var uri = 'mongodb+srv://wlto:myClusterIsFunny009@cluster0-o3gou.mongodb.net/test';
+var uri = 'mongodb://wlto:somethingFunny333@ds231758.mlab.com:31758/fishkatsu';
 
 // Connects to the database
 MongoClient.connect(uri, (err, client) => {
   // Getting the collection
-  const collection = client.db('Students').collection('students');
-  let stuffs = collection.find({}).toArray((err, docs) => {
-    console.log(docs); // Got the records!!!!!!!!!
-  });
+  const collection = client.db('fishkatsu').collection('Reports');
+  // let stuffs = collection.find({}).toArray((err, docs) => {
+  //   console.log(docs); // Got the records!!!!!!!!!
+  // });
   // Close the connection (I think)
   client.close();
 });
