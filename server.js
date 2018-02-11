7d4202ca43cea335db999c4d1c8e4e4face9395e
William To <williamto142@gmail.com>
2018-02-10 21:45:37 -0500

    Fixed stuffs

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+var path = require("path");
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-var path = require("path");
 var app = express();
 var HTTP_PORT = process.env.PORT || 8080;
 
 var uri = 'mongodb://wlto:somethingFunny333@ds231758.mlab.com:31758/fishkatsu';
 
-app.use(express.static('style'));
+app.use(express.static('public'));
 
 var documents;
 
@@ -21,20 +21,27 @@ MongoClient.connect(uri, (err, client) => {
   client.close();
 });
 
+// //////////
+// GET routes
+//
+
 // Route to home
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "/views/index.html"));
-  // res.send(documents);
-  // res.end();
 });
 
+// Route to everything else; Returns 404 Not Found
+app.get('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// ///////////
+// POST routes
+//
 app.post('/submit', (req, res) => {
-  
+
 });
 
-app.listen(port, () => {
-  console.log('Server listening on port ' + port);
-});
 app.listen(HTTP_PORT, () => {
   console.log('Server listening on port ' + HTTP_PORT);
 });
